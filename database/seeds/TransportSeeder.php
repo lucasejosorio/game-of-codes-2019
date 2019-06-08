bdac2a07a12251b74c9744bc82829b84adf160a7
Rafael Souto <souto.rafaelb@gmail.com>
2019-06-08 20:58:28 -0300

    transport types seeder

---
+++ database/seeds/TransportSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+use App\Transport;
+use Illuminate\Database\Seeder;
+
+class TransportSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Transport::create([
+            'title' => 'Bicicleta'
+        ]);
+
+        Transport::create([
+            'title' => 'Caminhando'
+        ]);
+
+    }
+}
