7a2e6a3cc1ddaa9d2bfcef768bb4f84094524fbe
Rafael Souto <souto.rafaelb@gmail.com>
2019-06-08 22:30:17 -0300

    rides controller with resource

--- app/Http/Controllers/RidesController.php
+++ app/Http/Controllers/RidesController.php
@@ -2,20 +2,99 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreRide;
 use App\Ride;
+use Illuminate\Contracts\Auth\Factory;
 use Illuminate\Http\Request;
+use Illuminate\View\View;
 
 class RidesController extends Controller
 {
-    public function create(){
-        $user = auth()->user();
+    private $auth;
+
+    /**
+     * RidesController constructor.
+     * @param Factory $auth
+     */
+    public function __construct(Factory $auth)
+    {
+        $this->auth = $auth;
+    }
+
+    /**
+     * return a view to create a new ride
+     * @return View
+     */
+    public function create()
+    {
+        $user = $this->auth->user();
+
         return view('rides.new', compact('user'));
     }
 
-    public function show($ride_id){
-        $user = auth()->user();
-        $ride = Ride::find($ride_id);
+    /**
+     * update a given ride resource
+     *
+     * @param StoreRide $request
+     * @return \Illuminate\Contracts\View\Factory|View
+     */
+    public function store(StoreRide $request)
+    {
+        $ride = Ride::create($request->validated());
+
+        if (!$ride) {
+            return redirect()
+                ->back()
+                ->withErrors([
+                    'message' => 'whoops, ocorreu um erro ao criar sua corrida',
+                ]);
+        }
+
+        return view('rides.show', compact('ride'));
+    }
+
+
+    /**
+     * Show a given ride
+     *
+     * @param $id
+     * @return \Illuminate\Contracts\View\Factory|View
+     */
+    public function show($id)
+    {
+        $user = $this->auth->user();
+
+        $ride = Ride::find($id);
         
         return view('rides.show', compact('user', 'ride'));
     }
+
+    /**
+     * A user can edit a given ride
+     *
+     * @param $id
+     * @return \Illuminate\Contracts\View\Factory|View
+     */
+    public function edit($id)
+    {
+        $ride = Ride::find($id);
+
+        return view('rides.show', compact('ride'));
+    }
+
+    /**
+     * update a given ride resource
+     *
+     * @param StoreRide $request
+     * @param $id
+     * @return \Illuminate\Contracts\View\Factory|View
+     */
+    public function update(StoreRide $request, $id)
+    {
+        $ride = Ride::where('id', $id)
+                    ->update($request->validated());
+
+        return view('rides.show', compact('ride'));
+    }
+
 }
